7d6b96f261d80ecde7a4eecd5c0920bf6230b78e
everyx <lunt.luo@gmail.com>
2014-12-06 00:19:04 +0800

    开放 png 格式

--- public/js/main.js
+++ public/js/main.js
@@ -11,19 +11,17 @@ $(document).ready(function() {
     var result;
     switch(outType) {
       case 'png':
-        // result = '<img src="' + url + '"></img>';
-        result = '该选项暂不支持，请看右侧说明。';
-        // $('#result').html(result + "<br> 链接：" + url);
-        $('#result').html(result);
+        result = '<img src="' + url + '"></img>';
+        $('#result').html(result + "<br> png 外链地址：" + url);
         break;
       case 'svg':
         result = '<object data="'+ url + '" type="image/svg+xml"></object>';
-        $('#result').html(result + "<br> svg 资源链接：" + url);
+        $('#result').html(result + "<br> svg 外链地址：" + url);
         break;
       case 'mml':
         $.get(url, function(data){
           result = data;
-          $('#result').html(result + "<br> mml 链接：" + url);
+          $('#result').html(result + "<br> mml 内容获取地址：" + url);
         });
         break;
       default:
